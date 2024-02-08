2e390abca790dc5fc0bef739f02a48ab1592180d
Rucha Khopkar <70127769+ruchakhopkar@users.noreply.github.com>
2024-02-08 07:46:05 -0600

    Update and rename 1346_Check_If_N_and_Its_Double_Exist to 1346_Check_If_N_and_Its_Double_Exist.py
    Binary Search

--- 1346_Check_If_N_and_Its_Double_Exist.py
+++ 1346_Check_If_N_and_Its_Double_Exist.py
@@ -1,17 +1,19 @@
+#sort array
+#keep double value as target
+#binary search
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         arr = sorted(arr)
         for i in range(len(arr)):
             target = arr[i]*2 
-            lptr = i+1
+            lptr = 0
             rptr = len(arr)-1
             while lptr<=rptr:
                 mid = (lptr + rptr)//2
-                if arr[mid]==target:
+                if (arr[mid]==target) and (mid!=i):
                     return True
                 elif arr[mid]>target:
                     rptr = mid-1
                 else:
                     lptr = mid+1
         return False
-        
